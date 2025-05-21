132345893605bd09a82cda47c78059958d792ce4
Pavithra8805 <pavithra8805@gmail.com>
2025-05-21 17:33:41 +0530

    Updated hello.js

--- hello.js
+++ hello.js
@@ -5,12 +5,18 @@ function sayHello(name) {
 
 // Greet based on time of day
 function timeBasedGreeting() {
-    const hour = new Date().getHours();
+    const hour = new Date().getHours(); 
     let greeting;
 
-    if (hour < 12) greeting = "☀️ Good morning";
-    else if (hour < 18) greeting = "🌤️ Good afternoon";
-    else greeting = "🌙 Good evening";
+    if (hour >= 5 && hour < 12) {
+        greeting = "☀️ Good morning";       
+    } else if (hour >= 12 && hour < 16) {
+        greeting = "🌤️ Good afternoon";     
+    } else if (hour >= 16 && hour < 19) {
+        greeting = "🌇 Good evening";        
+    } else {
+        greeting = "🌙 Good night";          
+    }
 
     return `${greeting}!`;
 }
